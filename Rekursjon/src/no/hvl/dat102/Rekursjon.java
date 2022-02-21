eb0da3fedbcfd1af8b0dcd1259f0fc939ed8e434
Jørgen Fjølstad <602622@stud.hvl.no>
2022-02-21 12:43:25 +0100

    Rekusjonsoppg lagt til i repo

---
+++ Rekursjon/src/no/hvl/dat102/Rekursjon.java
@@ -0,0 +1,79 @@
+package no.hvl.dat102;
+
+public class Rekursjon {
+
+    // a)
+    public static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n + sum(n - 1);
+        }
+    }
+
+    // b)
+    public static int aSubn(int n) {
+        if (n == 0) {
+            return 2;
+        }
+
+        if (n == 1) {
+            return 5;
+        }
+
+        return 5 * (aSubn(n - 1)) - 6 * (aSubn(n - 2)) + 2;
+    }
+
+    // c)
+    public static int fn(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        return fn(n - 2) + fn(n - 1);
+    }
+
+    // d)
+    public static int fnUtenRekursjon(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        int prev1 = 0;
+        int prev2 = 1;
+        int sum = 0;
+
+        for (int i = 2; i <= n; i++) {
+            sum = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = sum;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("a)\n S(100) = " + sum(100) + "\n");
+
+        System.out.println("b)");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(aSubn(i));
+        }
+        System.out.println();
+
+        System.out.println("c)");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(fn(i));
+        }
+        System.out.println();
+
+        System.out.println("d)\n" + fnUtenRekursjon(10));
+    }
+}
